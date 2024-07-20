b5eb1bc5644f4696e6ef70e6d2143a831fc7a63d
Vipul Kumar Chauhan <vc252@snu.edu.in>
2024-07-20 18:58:10 +0530

    add artithematic operations

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let operatorPressed = false;
+let num2Pressed = false;
+let num1Pressed = false;
 let num1 = 0;
 let num2 = 0;
+let operator;
 let result = 0;
 const screen = document.querySelector(".screen");
 //if i press operator after num1
@@ -11,17 +14,32 @@ const screen = document.querySelector(".screen");
 const numbers = document.querySelectorAll(".num");
 numbers.forEach((num)=>{
     num.addEventListener("click",(e)=>{
+        if (num1Pressed) {
+            num2Pressed = true;
+        }
         if (operatorPressed) {
             clearScreen();
             operatorPressed = false;
         }
-        printOnScreen(e.target);
+        pushOnScreen(e.target);
     })
 })
 
 const operators = document.querySelectorAll(".operator");
 operators.forEach((op)=>{
-    op.addEventListener("click",()=>{
+    op.addEventListener("click",(e)=>{
+        if (num2Pressed) {
+            num2 = getNumberFromScreen();
+            result = calculate();
+            displayResult();
+            num1 = result;
+            num2Pressed = false;
+        }
+        if (!operatorPressed && !num1Pressed) {
+            num1Pressed = true;
+            num1 = getNumberFromScreen();
+        }
+        operator = e.target.classList[0];
         operatorPressed = true;
     })
 })
@@ -39,7 +57,7 @@ deleteButton.addEventListener("click",()=>{
     screen.textContent = str.join('');
 })
 
-function printOnScreen(num) {
+function pushOnScreen(num) {
     screen.textContent += num.textContent;
 }
 
@@ -50,6 +68,24 @@ function getNumberFromScreen() {
 
 function clearScreen() {
     screen.textContent = "";
+}
+
+function calculate() {
+    switch(operator) {
+        case 'divide':
+            return num1/num2;
+        case 'multilply':
+            return num1*num2;
+        case 'minus':
+            return num1-num2;
+        case 'plus':
+            return num1+num2;
+    }
+}
+
+function displayResult() {
+    clearScreen();
+    screen.textContent = result;
 }
 
 
