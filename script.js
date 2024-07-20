160fc027b0a2f1e003e2c5f871a93aea9f354abb
Vipul Kumar Chauhan <vc252@snu.edu.in>
2024-07-20 16:53:59 +0530

    add clear function
    clear screen after pressing the clear button or if
    we start typing after pressing an operator

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-let numbers = document.querySelectorAll(".num");
+let operatorPressed = false;
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+const screen = document.querySelector(".screen");
+//if i press operator after num1
+//then nothing will change
+//but after both have been entered
+//result is declared
+
+const numbers = document.querySelectorAll(".num");
 numbers.forEach((num)=>{
     num.addEventListener("click",(e)=>{
+        if (operatorPressed) {
+            clearScreen();
+            operatorPressed = false;
+        }
         printOnScreen(e.target);
     })
 })
 
+const operators = document.querySelectorAll(".operator");
+operators.forEach((op)=>{
+    op.addEventListener("click",()=>{
+        operatorPressed = true;
+    })
+})
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click",()=>{
+    clearScreen();
+})
+
 function printOnScreen(num) {
-    const screen = document.querySelector(".screen");
     screen.textContent += num.textContent;
 }
+
+function getNumberFromScreen() {
+    let number = screen.textContent;
+    return parseInt(number);
+}
+
+function clearScreen() {
+    screen.textContent = "";
+}
+
 
